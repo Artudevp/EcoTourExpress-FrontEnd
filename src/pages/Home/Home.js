76a4f6f3809c4b7b591015849f04d4d0c0c8c723
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-08-11 18:47:16 -0500

    Se importa y usa el elemento Link de React Router

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import { Panel } from 'primereact/panel';
+import { Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
-import '../../App.css'; 
+import '../../App.css';
 
 const Home = () => {
+    const buttons = [
+        {
+            label: "Ir a Usuarios",
+            icon: "pi pi-user",
+            link: "/usuarios",
+            className: "p-button-success"
+        },
+        {
+            label: "Ir a Clientes",
+            icon: "pi pi-users",
+            link: "/clientes",
+            className: "p-button-info"
+        },
+        {
+            label: "Ir a Actividades",
+            icon: "pi pi-calendar",
+            link: "/actividades",
+            className: "p-button-warning"
+        },
+        {
+            label: "Ir a Productos",
+            icon: "pi pi-box",
+            link: "/productos",
+            className: "p-button-primary"
+        },
+        {
+            label: "Ir a Rutas",
+            icon: "pi pi-map",
+            link: "/rutas",
+            className: "p-button-secondary"
+        },
+        {
+            label: "Ir a Hospedaje",
+            icon: "pi pi-home",
+            link: "/hospedaje",
+            className: "p-button-secondary"
+        },
+    ]
     return (
         <div style={{ width: '80%', margin: '20px auto 0px' }}>
             <Panel header="Bienvenido a la Gestión de Datos de EcoTourExpress (CRUD)" style={{ textAlign: 'center' }}>
@@ -12,12 +51,13 @@ const Home = () => {
                     Esta es tu aplicación para gestionar usuarios, clientes, actividades, productos y rutas.
                 </p>
                 <div style={{ textAlign: 'center' }}>
-                    <Button label="Ir a Usuarios" icon="pi pi-user" className="p-button-success" onClick={() => window.location.href = '/usuarios'} />
-                    <Button label="Ir a Clientes" icon="pi pi-users" className="p-button-info" onClick={() => window.location.href = '/clientes'} />
-                    <Button label="Ir a Actividades" icon="pi pi-calendar" className="p-button-warning" onClick={() => window.location.href = '/actividades'} />
-                    <Button label="Ir a Productos" icon="pi pi-box" className="p-button-primary" onClick={() => window.location.href = '/productos'} />
-                    <Button label="Ir a Rutas" icon="pi pi-map" className="p-button-secondary" onClick={() => window.location.href = '/rutas'} />
-                    <Button label="Ir a Hospedaje" icon="pi pi-home" className="p-button-secondary" onClick={() => window.location.href = '/hospedaje'} />
+                    {
+                        buttons.map((button, index) => (
+                            <Link to={button.link} key={index}>
+                                <Button label={button.label} icon={button.icon} className={button.className} />
+                            </Link>
+                        ))
+                    }
                 </div>
             </Panel>
         </div>
